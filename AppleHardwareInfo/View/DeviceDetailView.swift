2f4acfbfd380678ef6f68ab76e4d004098ba4126
yu3san3 <orange12817@yahoo.co.jp>
2023-12-13 19:32:50 +0900

    DeviceDetailViewをリファクタした。

--- AppleHardwareInfo/View/DeviceDetailView.swift
+++ AppleHardwareInfo/View/DeviceDetailView.swift
@@ -8,185 +8,190 @@
 import SwiftUI
 
 struct DeviceDetailView: View {
-    
+
     var device: DeviceData
 
-    let technicalSpecificationsUrl: URL?
     @State private var batteryCapacityUnitDisplayMode: BatteryCapacityUnit
-
-    let batteryVoltage = 3.82
     @State private var timeDisplayMode: TimeDisplayMode = .monthDay
+
     @State private var shouldShowWebView: Bool = false
     @State private var shouldShowGlossaryView: Bool = false
 
     init(device: DeviceData) {
         self.device = device
-        //技術仕様のURLを完成させる
-        self.technicalSpecificationsUrl = URL(
-            string: "https://support.apple.com/\(device.technicalSpecificationsUrl)"
-        )
         self._batteryCapacityUnitDisplayMode = State(initialValue: device.battery.unit)
     }
-    
+
     var body: some View {
         List {
-            Section {
-                SplitTextListItem(title: "SoC", element: device.chip.chipName)
-                SplitTextListItem(title: "プロセスルール", element: device.chip.manufacturingProcess)
-                SplitTextListItem(title: "CPUコア数", element: device.chip.cpuCoreNum)
-                SplitTextListItem(title: "GPUコア数", element: device.chip.gpuCoreNum)
-                if let neuralEngineCoreNum = device.chip.neuralEngineCoreNum {
-                    SplitTextListItem(title: "Neural Engineコア数", element: neuralEngineCoreNum)
-                }
-            } header: {
-                Text("チップ")
-            } footer: {
-                Text("P: Performance  E: Efficiency")
-            }
-            Section {
-                SplitTextListItem(title: "容量", element: device.memoryCapacity)
-            } header: {
-                Text("メモリ")
-            }
-            Section {
-                SplitTextListItem(title: "サイズ", element: device.dispInch.localizedNumber)
-                SplitTextListItem(title: "解像度", element: device.dispResolution.localizedNumber)
-                SplitTextListItem(title: "画素密度", element: device.dispPpi)
-            } header: {
-                Text("ディスプレイ")
-            }
-            Section {
-                if let rearCamWide = device.rearCam[.wide] {
-                    SplitTextListItem(title: "広角", element: rearCamWide)
-                }
-                if let rearCamUltraWide = device.rearCam[.ultraWide] {
-                    SplitTextListItem(title: "超広角", element: rearCamUltraWide)
-                }
-                if let rearCamTele = device.rearCam[.tele] {
-                    SplitTextListItem(title: "望遠", element: rearCamTele)
-                }
-            } header: {
-                Text("背面カメラ")
-            }
-            Section {
-                if let frontCamWide = device.frontCam[.wide] {
-                    SplitTextListItem(title: "広角", element: frontCamWide)
-                }
-                if let frontCamUltraWide = device.frontCam[.ultraWide] {
-                    SplitTextListItem(title: "超広角", element: frontCamUltraWide)
-                }
-            } header: {
-                Text("前面カメラ")
-            }
-            Section {
-                SplitTextListItem(title: "重量", element: device.weight)
-                makeBatteryCapacityListItem()
-                makeReleaseDateListItem()
-            } header: {
-                Text("その他")
-            }
-            Section {
-                makeLinkToTechnicalSpecification()
-            } header: {
-                Text("リンク")
-            } footer: {
-                Text("リーダーを表示すると読みやすくなります。")
-            }
+            chipSection
+            memorySection
+            displaySection
+            rearCameraSection
+            frontCameraSection
+            othersSection
+            linkSection
         }
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
-                glossaryButton
-                    .sheet(isPresented: $shouldShowGlossaryView) {
-                        GlossaryView() //用語集
-                    }
+                Button {
+                    shouldShowGlossaryView = true
+                } label: {
+                    Image(systemName: "info.circle")
+                }
+                .sheet(isPresented: $shouldShowGlossaryView) {
+                    GlossaryView() //用語集
+                }
             }
         }
         .navigationTitle(device.deviceName)
         .navigationBarTitleDisplayMode(.inline)
     }
+
+    var chipSection: some View {
+        Section {
+            SplitTextListItem(title: "SoC", element: device.chip.chipName)
+            SplitTextListItem(title: "プロセスルール", element: device.chip.manufacturingProcess)
+            SplitTextListItem(title: "CPUコア数", element: device.chip.cpuCoreNum)
+            SplitTextListItem(title: "GPUコア数", element: device.chip.gpuCoreNum)
+            if let neuralEngineCoreNum = device.chip.neuralEngineCoreNum {
+                SplitTextListItem(title: "Neural Engineコア数", element: neuralEngineCoreNum)
+            }
+        } header: {
+            Text("チップ")
+        } footer: {
+            Text("P: Performance  E: Efficiency")
+        }
+    }
+
+    var memorySection: some View {
+        Section {
+            SplitTextListItem(title: "容量", element: device.memoryCapacity)
+        } header: {
+            Text("メモリ")
+        }
+    }
+
+    var displaySection: some View {
+        Section {
+            SplitTextListItem(title: "サイズ", element: device.dispInch.localizedNumber)
+            SplitTextListItem(title: "解像度", element: device.dispResolution.localizedNumber)
+            SplitTextListItem(title: "画素密度", element: device.dispPpi)
+        } header: {
+            Text("ディスプレイ")
+        }
+    }
+
+    var rearCameraSection: some View {
+        Section {
+            if let rearCamWide = device.rearCam[.wide] {
+                SplitTextListItem(title: "広角", element: rearCamWide)
+            }
+            if let rearCamUltraWide = device.rearCam[.ultraWide] {
+                SplitTextListItem(title: "超広角", element: rearCamUltraWide)
+            }
+            if let rearCamTele = device.rearCam[.tele] {
+                SplitTextListItem(title: "望遠", element: rearCamTele)
+            }
+        } header: {
+            Text("背面カメラ")
+        }
+    }
+
+    var frontCameraSection: some View {
+        Section {
+            if let frontCamWide = device.frontCam[.wide] {
+                SplitTextListItem(title: "広角", element: frontCamWide)
+            }
+            if let frontCamUltraWide = device.frontCam[.ultraWide] {
+                SplitTextListItem(title: "超広角", element: frontCamUltraWide)
+            }
+        } header: {
+            Text("前面カメラ")
+        }
+    }
+
+    var othersSection: some View {
+        Section {
+            SplitTextListItem(title: "重量", element: device.weight)
+            let capacityStr = getAppropriateBatteryCapacityStr()
+            SplitTextListItem(title: "バッテリー容量", element: capacityStr.localizedNumber)
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        batteryCapacityUnitDisplayMode.toggle()
+                    }
+            let element = getFormattedReleaseDateStr()
+            SplitTextListItem(title: "発売日", element: element)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    timeDisplayMode.toggle()
+                }
+        } header: {
+            Text("その他")
+        }
+    }
+
+    var linkSection: some View {
+        Section {
+            if let technicalSpecificationsUrl = URL(
+                string: "https://support.apple.com/\(device.technicalSpecificationsUrl)"
+            ) {
+                HStack {
+                    Text("技術仕様 (support.apple.com)")
+                        .foregroundColor(.blue)
+                    Spacer()
+                }
+                .contentShape(Rectangle()) //セル全体をタップ領域にする
+                .onTapGesture {
+                    shouldShowWebView.toggle()
+                }
+                .fullScreenCover(isPresented: $shouldShowWebView) {
+                    SafariView(url: technicalSpecificationsUrl)
+                        .edgesIgnoringSafeArea(.all)
+                }
+            } else {
+                Text("Error: Invalid link to technical specification.")
+            }
+        } header: {
+            Text("リンク")
+        } footer: {
+            Text("リーダーを表示すると読みやすくなります。")
+        }
+    }
 }
 
 private extension DeviceDetailView {
-    var glossaryButton: some View {
-        Button(action: {
-            shouldShowGlossaryView = true
-        }) {
-            Image(systemName: "info.circle")
-        }
-    }
-
-    @ViewBuilder
-    func makeBatteryCapacityListItem() -> some View {
+    func getAppropriateBatteryCapacityStr() -> String {
+        let batteryVoltage = 3.82
         let capacity = device.battery.capacity
-        let unit = device.battery.unit
-        let element = getConvertedElement(capacity: capacity,
-                                          unit: unit,
-                                          displayMode: batteryCapacityUnitDisplayMode)
-        SplitTextListItem(title: "バッテリー容量", element: element.localizedNumber)
-            .contentShape(Rectangle())
-            .onTapGesture {
-                batteryCapacityUnitDisplayMode.toggle()
-            }
-    }
-
-    func getConvertedElement(capacity: Double, unit: BatteryCapacityUnit, displayMode: BatteryCapacityUnit) -> String {
-        if unit == displayMode {
-            return "\(capacity) \(displayMode.rawValue)"
+        let registeredUnit = device.battery.unit
+        let displayMode = self.batteryCapacityUnitDisplayMode
+        //登録されている単位(registeredUnit)と表示モード(displayMode)が同じなら、データ通りの値を返す。
+        if registeredUnit == displayMode {
+            return "\(capacity) \(registeredUnit.rawValue)"
         }
-        switch unit {
-        case .mAh:
-            let calculated = (capacity * batteryVoltage)/1000 //mAh -> Wh
-            let formatted = String(format: "%.2f", calculated)
+        //そうでなければ単位を変換
+        switch registeredUnit {
+        case .mAh: //mAh -> Wh
+            let converted = (capacity * batteryVoltage)/1000
+            let formatted = String(format: "%.2f", converted)
             return "約 \(formatted) \(displayMode.rawValue)"
-        case .wh:
-            let calculated = round( (capacity * 1000)/batteryVoltage ) //Wh -> mAh
-            return "約 \(calculated) \(displayMode.rawValue)"
+        case .wh: //Wh -> mAh
+            let converted = round( (capacity * 1000)/batteryVoltage )
+            return "約 \(converted) \(displayMode.rawValue)"
         }
     }
 
-    @ViewBuilder
-    func makeReleaseDateListItem() -> some View {
-        let element = getReleaseDateElement()
-        SplitTextListItem(title: "発売日", element: element)
-            .contentShape(Rectangle())
-            .onTapGesture {
-                timeDisplayMode.toggle()
-            }
-    }
-
-    func getReleaseDateElement() -> String {
-        switch timeDisplayMode {
+    func getFormattedReleaseDateStr() -> String {
+        switch self.timeDisplayMode {
         case .monthDay:
             return "\(device.releaseDate.localizedDate.monthDay)"
         case .daysElapsed:
             return "\(device.releaseDate.localizedDate.daysElapsed) 前"
         }
     }
-
-    @ViewBuilder
-    func makeLinkToTechnicalSpecification() -> some View {
-        if let url = technicalSpecificationsUrl {
-            HStack {
-                Text("技術仕様 (support.apple.com)")
-                    .foregroundColor(.blue)
-                Spacer()
-            }
-            .contentShape(Rectangle()) //セル全体をタップ領域にする
-            .onTapGesture {
-                shouldShowWebView.toggle()
-            }
-            .fullScreenCover(isPresented: $shouldShowWebView) {
-                SafariView(url: url)
-                    .edgesIgnoringSafeArea(.all)
-            }
-        } else {
-            Text("Error: Invalid link to technical specification.")
-        }
-    }
 }
 
-struct DeviceDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DeviceDetailView(device: DeviceData.iPhoneArray[0])
-    }
+#Preview {
+    DeviceDetailView(device: DeviceData.iPhoneArray[0])
 }
